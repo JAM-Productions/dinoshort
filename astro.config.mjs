b2e95d1fa65d1ab806b1b508ce3f237a6118d276
Jordi <55429631+jorbush@users.noreply.github.com>
2024-05-30 08:05:34 +0200

    Init component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
