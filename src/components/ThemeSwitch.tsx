be6831439f3038b1cc0d95145799ddaa9ff12ef5
Jordi <55429631+jorbush@users.noreply.github.com>
2024-06-16 16:23:12 +0200

    Format

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import './ThemeSwitch.css';
+import React, { useState, useEffect } from "react";
+import "./ThemeSwitch.css";
 
 const ThemeSwitch: React.FC = () => {
-  const [darkMode, setDarkMode] = useState<boolean>(() => {
-    const savedMode = window.localStorage.getItem('darkMode');
-    return savedMode === 'true';
-  });
+    const [darkMode, setDarkMode] = useState<boolean>(() => {
+        const savedMode = window.localStorage.getItem("darkMode");
+        return savedMode === "true";
+    });
 
-  useEffect(() => {
-    const root = document.documentElement;
-    if (darkMode) {
-      root.classList.add('dark');
-    } else {
-      root.classList.remove('dark');
-    }
-    window.localStorage.setItem('darkMode', darkMode.toString());
-  }, [darkMode]);
+    useEffect(() => {
+        const root = document.documentElement;
+        if (darkMode) {
+            root.classList.add("dark");
+        } else {
+            root.classList.remove("dark");
+        }
+        window.localStorage.setItem("darkMode", darkMode.toString());
+    }, [darkMode]);
 
-  return (
-    <label className="switch">
-      <input
-        type="checkbox"
-        checked={darkMode}
-        onChange={() => setDarkMode(!darkMode)}
-        className="hidden"
-      />
-      <span className="slider round"></span>
-    </label>
-  );
+    return (
+        <label className="switch">
+            <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+                className="hidden"
+            />
+            <span className="slider round"></span>
+        </label>
+    );
 };
 
 export default ThemeSwitch;
