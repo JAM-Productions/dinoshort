15fcf562afd84d1ad0146b3602e46ca43a5b5628
Jordi <55429631+jorbush@users.noreply.github.com>
2024-06-16 16:18:38 +0200

    Implement ThemeSwitch component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				accent: 'rgb(136, 58, 234)',
+				'accent-light': 'rgb(224, 204, 250)',
+				'accent-dark': 'rgb(49, 10, 101)',
+			},
+		},
 	},
 	plugins: [],
 }
