e94f67bf17fd999486068c2367db167a94213b27
Jordi <55429631+jorbush@users.noreply.github.com>
2024-06-16 15:54:59 +0200

    integrate react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    svelte()
+    svelte(),
+    react()
   ]
 });
